a582310262b7e46591e9872fded2c29bc0395d88
Benjamin Luke Smith-Gordon <ben.smith.gordon@gmail.com>
2020-06-15 17:33:49 +0100

    Changed addTodo, changeTodo, and added function toggleCompleted
    addTodo now pushes an object into the todos array rather than a string, changeTodo reflects the changes made to addTodo and now takes a todoTextValue as an additional argument. Also added a new property and function called toggleCompleted which takes an argument of position which refers to an element in the todos array, and changes that element's completed status to the opposite of what it currently is.

--- js/app.js
+++ js/app.js
@@ -1,20 +1,27 @@
 
 let todoList = {
   todos: [], //A list of todos.
-  
   displayTodos: function() { //It should have a function to display todos.
     console.log(this.todos);
   },
-  addTodo: function(todo) { //It should have a function to add todos.
-    this.todos.push(todo)
+  addTodo: function(todoTextValue) { //It should have a function to add todos.
+    this.todos.push({
+      todoText: todoTextValue,
+      completed: false
+    });
     this.displayTodos();
   },
-  changeTodo: function(position, newValue) { //It should have a function to change todos.
-    this.todos[position] = newValue;
+  changeTodo: function(position, todoTextValue) { //It should have a function to change todos.
+    this.todos[position].todoText = todoTextValue;
     this.displayTodos();
   },
   deleteTodo: function(position) { //It should have a function to delete todos.
     this.todos.splice(position, 1);
+    this.displayTodos();
+  },
+  toggleCompleted: function(position) { //It should have a function that changes the completed property from false to true.
+    let todo = this.todos[position];
+    todo.completed = !todo.completed;
     this.displayTodos();
   }
 };
