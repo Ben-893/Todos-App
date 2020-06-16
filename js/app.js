fb501f4d9775d52b592aba4c0d8817b327516f70
Benjamin Luke Smith-Gordon <ben.smith.gordon@gmail.com>
2020-06-16 20:02:53 +0100

    Changed functionality of displayTodos,
    Changed the way the displayTodos method outputs data by adding an 'if else' statement at the beginning of the method to check if the todos array's length is equal to '0', and if so to print a message to the console, else to run a loop which iterates over each item in the array and checks whether or not each element has a completed status of true, in which case it prints a '(x)' symbol next to the element. If the completed status is equal to false, it will print a symbol '( )' next to the element.

--- js/app.js
+++ js/app.js
@@ -2,7 +2,18 @@
 let todoList = {
   todos: [], //A list of todos.
   displayTodos: function() { //It should have a function to display todos.
-    console.log(this.todos);
+    if (this.todos.length === 0) {
+      console.log('The list is empty, you have nothing you need to do!'); //Added a condition to to check whether or not the array is empty.
+    } else {
+      console.log('My Todos:');
+      for (let i = 0; i < this.todos.length; i++) { //Created a 'for' loop to itterate through the elements of the todos array.
+        if (this.todos[i].completed === true) { //Added a conditional statement to display a check mark on elements marked 'completed', or vice-vera.
+          console.log('(x)', this.todos[i].todoText);
+        } else {
+          console.log('( )', this.todos[i].todoText);
+        }
+      }
+    }
   },
   addTodo: function(todoTextValue) { //It should have a function to add todos.
     this.todos.push({
