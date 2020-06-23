cc5502e87ecf26d5ae035d475af949f96ef5b58e
Benjamin Luke Smith-Gordon <ben.smith.gordon@gmail.com>
2020-06-23 18:22:24 +0100

    Added a 'view' object and moved the 'displayTodos' method into it
    Created a view object to render the 'todo' list to the web page in the form of bullet points, rather than the console.
    Removed the original 'displayTodos' method from the top of the code, and instead put an updated version of the method into the 'view' object.
    Created a '<ul>' element to hold items from the todos array in the form of <li> tags.

--- js/app.js
+++ js/app.js
@@ -1,39 +1,21 @@
 
 let todoList = {
   todos: [], //A list of todos.
-  displayTodos: function() { //It should have a function to display todos.
-    if (this.todos.length === 0) {
-      console.log('The list is empty, you have nothing you need to do!'); //Added a condition to to check whether or not the array is empty.
-    } else {
-      console.log('My Todos:');
-      for (let i = 0; i < this.todos.length; i++) { //Created a 'for' loop to itterate through the elements of the todos array.
-        if (this.todos[i].completed === true) { //Added a conditional statement to display a check mark on elements marked 'completed', or vice-vera.
-          console.log('(x)', this.todos[i].todoText);
-        } else {
-          console.log('( )', this.todos[i].todoText);
-        }
-      }
-    }
-  },
   addTodo: function(todoTextValue) { //It should have a function to add todos.
     this.todos.push({
       todoText: todoTextValue,
       completed: false
     });
-    this.displayTodos();
   },
   changeTodo: function(position, todoTextValue) { //It should have a function to change todos.
     this.todos[position].todoText = todoTextValue;
-    this.displayTodos();
   },
   deleteTodo: function(position) { //It should have a function to delete todos.
     this.todos.splice(position, 1);
-    this.displayTodos();
   },
   toggleCompleted: function(position) { //It should have a function that changes the completed property from false to true.
     let todo = this.todos[position];
     todo.completed = !todo.completed;
-    this.displayTodos();
   },
   toggleAll: function() {
     let totalTodos = this.todos.length;
@@ -53,18 +35,15 @@ let todoList = {
       for (let i = 0; i < totalTodos; i++)
         this.todos[i].completed = true;
     }
-    this.displayTodos();
   }
 };
 
 let handlers = {
-  displayTodos: () => {                                  
-    todoList.displayTodos();
-  },
   addTodo: () => {
     let addTodoTextInput = document.getElementById('addTodoTextInput');
     todoList.addTodo(addTodoTextInput.value);
     addTodoTextInput.value = '';
+    view.displayTodos();
   },
   changeTodo: () => {
     let changeTodoPositionInput = document.getElementById('changeTodoPositionInput');
@@ -72,22 +51,43 @@ let handlers = {
     todoList.changeTodo(changeTodoPositionInput.valueAsNumber, changeTodoTextInput.value)
     changeTodoPositionInput.value = '';
     changeTodoTextInput.value = '';
+    view.displayTodos();
   },
   deleteTodo: () => {
     let deleteTodoPositionInput = document.getElementById('deleteTodoPositionInput');
     todoList.deleteTodo(deleteTodoPositionInput.valueAsNumber);
     deleteTodoPositionInput.value = ''
+    view.displayTodos();
   },
   toggleCompleted: () => {
     let toggleCompletedPositionInput = document.getElementById('toggleCompletedPositionInput');
     todoList.toggleCompleted(toggleCompletedPositionInput.valueAsNumber);
     toggleCompletedPositionInput.value = ''
+    view.displayTodos();
   },
   toggleAll: () => {
     todoList.toggleAll();
+    view.displayTodos();
   }
 };
 
-/* Refacored the code that was handling the buttons, and created a 'handlers' object
-   to hold the 'todolist' functions so my html buttons 
-   can have access to them. */
+let view = {  //Created a view object to render the todo list to the webpage in the form of bullet points, rather than the console.
+  displayTodos: () => {     //removed the original 'displayTodos' method from the top of the code, and instead put an updated version of the method into the 'view' object.
+    let todosUl = document.querySelector('ul');
+    todosUl.innerHTML = '';
+    for (let i = 0; i < todoList.todos.length; i++) {
+      let todoLi = document.createElement('li');
+      let todo = todoList.todos[i];
+      let todoTextWithCompletion = '';
+      
+      if (todo.completed === true) {
+        todoTextWithCompletion = `(x) ${todo.todoText}`;
+      } else {
+        todoTextWithCompletion = `( ) ${todo.todoText}`;
+      }
+
+      todoLi.textContent = todoTextWithCompletion;
+      todosUl.appendChild(todoLi);
+    }
+  }
+};
